a73d03dc271adc4b9918891a57c4cfee94b7829c
Richard Neave <Richard.Neave@ipsoft.com>
2019-03-01 09:01:41 +0000

    update urls

--- 1desk-cli.rb
+++ 1desk-cli.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class AitCli < Formula
     homepage "https://ipsoft.com"
-    url "https://github.com/ipsoft-tools/ait-cli.git"
-    head "https://github.com/ipsoft-tools/ait-cli.git"
+    url "https://github.com/ipsoft-tools/1desk-cli.git"
+    head "https://github.com/ipsoft-tools/1desk-cli.git"
 
     depends_on "go" => :build
 
@@ -14,14 +14,14 @@ class AitCli < Formula
         system "go", "get", "github.com/mitchellh/go-homedir"
         system "go", "get", "github.com/spf13/cobra"
         system "go", "get", "github.com/spf13/viper"
-        system "go", "get", "github.com/ipsoft-tools/ait-cli/conf"
-        system "go", "get", "github.com/ipsoft-tools/ait-cli/cmd"
+        system "go", "get", "github.com/ipsoft-tools/1desk-cli/conf"
+        system "go", "get", "github.com/ipsoft-tools/1desk-cli/cmd"
         
-        system "go", "build", "-o", "ait"
-        bin.install "ait"
+        system "go", "build", "-o", "1desk"
+        bin.install "1desk"
     end
 
     test do
-        system "#{bin}/ait", "-v"
+        system "#{bin}/1desk", "-v"
     end
 end
